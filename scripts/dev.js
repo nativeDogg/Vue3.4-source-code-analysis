933072549e072931b31ed61868b29c1d9b16cf12
changeqingteng <790792491@qq.com>
2024-06-23 20:56:38 +0800

    update

--- scripts/dev.js
+++ scripts/dev.js
@@ -1,19 +1,19 @@
 // 此文件帮助我们打包packages下的模块 最终打包出js文件
 // node dev.js (要打包的名字 -f 打包的格式) == args.slice(2)
 import minimist from "minimist";
-import { resolve dirname } from 'path'
-import { fileURLToPath } from 'url'
-import { createRequire } from 'module'
+import { resolve, dirname } from "path";
+import { fileURLToPath } from "url";
+import { createRequire } from "module";
 // node中的命令行参数通过process 来获取process.argv
-const args = minimist(process.argv.slice(2))
-const target = args._[0] || 'reactivity'
-const format = args.f || 'iife'
+const args = minimist(process.argv.slice(2));
+const target = args._[0] || "reactivity";
+const format = args.f || "iife";
 // 获取文件的路径
-const __filename = fileURLToPath(import.meta.url)
+const __filename = fileURLToPath(import.meta.url);
 // 获取文件的文件夹路径 在node中esm模块没有 __dirname 需要用这种方法获取
-const __dirname = dirname(__filename)
+const __dirname = dirname(__filename);
 
-const require = createRequire(import.meta.url)
+const require = createRequire(import.meta.url);
 
 // 入口文件
-const entry = resolve(__dirname,`../packages/${target}/src/index.ts`)
+const entry = resolve(__dirname, `../packages/${target}/src/index.ts`);
