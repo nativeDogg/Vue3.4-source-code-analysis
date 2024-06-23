be6c46e51b85e7b0b733f736a400e6c293523329
changeqingteng <790792491@qq.com>
2024-06-23 21:08:43 +0800

    实现dev.js打包逻辑

--- scripts/dev.js
+++ scripts/dev.js
@@ -4,6 +4,7 @@ import minimist from "minimist";
 import { resolve, dirname } from "path";
 import { fileURLToPath } from "url";
 import { createRequire } from "module";
+import esbuild from "esbuild";
 // node中的命令行参数通过process 来获取process.argv
 const args = minimist(process.argv.slice(2));
 const target = args._[0] || "reactivity";
@@ -17,3 +18,23 @@ const require = createRequire(import.meta.url);
 
 // 入口文件
 const entry = resolve(__dirname, `../packages/${target}/src/index.ts`);
+const pkg = require(`../packages/${target}/package.json`);
+esbuild
+  .context({
+    // 入口
+    entryPoints: [entry],
+    // 出口
+    outfile: resolve(__dirname, `../packages/${target}/dist/${target}.js`),
+    // 当一个模块依赖另一个模块的时候 会把另一个模块也打包进去
+    bundle: true,
+    // 打包后给浏览器使用
+    platform: "browser",
+    sourcemap: true,
+    format,
+    globalName: pkg.buildOptions?.name,
+  })
+  .then((ctx) => {
+    console.log("start dev");
+    // 监控入口文件持续进行打包处理
+    return ctx.watch();
+  });
